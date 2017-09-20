deac651e581d49a1acd32271a1ef7965a555996e
joshlacey <williamjoshualacey@gmail.com>
2017-09-20 16:36:48 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
 // Code SimpleComponentHere Here
+import React from 'react'
+
+class SimpleComponent extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      mood: "happy"
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      mood: (this.state.mood == "happy") ? "sad" : "happy"
+    })
+  }
+
+  render () {
+    return (
+      <div onClick={this.handleClick}>{this.state.mood}</div>
+    )
+  }
+}
+
+export default SimpleComponent
+
+
+//Google maps object renders object that needs to be used for an api call.
